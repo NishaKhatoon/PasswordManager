b3b06ae843a589a04575537363c0259542e1e961
Rounpay <76778687+Rounpay@users.noreply.github.com>
2024-07-04 22:28:34 +0530

    Updated

--- app/src/main/java/com/test/passwordmanagerapplication/ui/NavDestination.kt
+++
@@ -1,9 +0,0 @@
-package com.test.passwordmanagerapplication.ui
-
-sealed class NavDestination(val route: String) {
-
-    data object PasswordList : NavDestination("passwordList")
-    data object AddEditPassword : NavDestination("addEditPassword/{passwordId}") {
-        fun createRoute(passwordId: Int) = "addEditPassword/$passwordId"
-    }
-}
